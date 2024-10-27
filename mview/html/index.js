0c0401310a7798968166ce993b0a351a7b9f8836
yahsaki <github@yahsaki.com>
2024-10-26 21:02:17 -0700

    mview: split lower data controls in same format as videos, check if playback is occurring and set toggle accordingly

--- mview/html/index.js
+++ mview/html/index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs')
 const path = require('node:path')
 
 // dont even need async
+// this couldve been a bat file but I plan on implementing more features that are a pain with scripting
 ;(async () => {
   const filePaths = fs.readdirSync('./')
   let datajs = '//This file is auto generated\nconst data = '
@@ -13,4 +14,5 @@ const path = require('node:path')
   }
   datajs += JSON.stringify(files, ' ', 2)
   fs.writeFileSync('data.js', datajs)
+  console.log(`${files.length} files set`)
 })()
---
+++ mview/html/readme.txt
@@ -0,0 +1,5 @@
+how to use:
+- put video files and these files in same directory
+- run that convert bat file if they arent mp4s(webm support not coming, too much potential bloat)
+- run index.js to create a list of mp4s for the html file to consume(requires ffmpeg globally accessible)
+- open index.html file(only tested chrome and firefox)
